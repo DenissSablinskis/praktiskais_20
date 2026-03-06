979908bc24d52193653698168072eeb7cb6a1536
Deniss <denissablinskij@gmail.com>
2026-03-06 09:33:20 +0100

    Pievienota jauna funkcionalitate

--- app.py
+++ app.py
@@ -4,3 +4,8 @@
 def izvilkt_kvadratsakni(skaitlis):
     """Atgriež kvadrātsaknes vertību"""
     return skaitlis ** (1/2)
+
+
+def saskaitit(a, b):
+    """Atgriež divu skaitļu summu"""
+    return a + b
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 
-from app import izvilkt_kvadratsakni
+from app import izvilkt_kvadratsakni, saskaitit
+
 
 def test_izvilkt_kvadratsakni_25():
     assert izvilkt_kvadratsakni(25) == 5
@@ -9,4 +10,13 @@ def test_izvilkt_kvadratsakni_36():
 
 def test_izvilkt_kvadratsakni_49():
     assert izvilkt_kvadratsakni(49) == 7
+
+def test_saskaitit():
+    assert saskaitit(5, 5) == 10
+
+def test_saskaitit():
+    assert saskaitit(10, 5) == 15
+
+def test_saskaitit():
+    assert saskaitit(3, 9) == 12
 
